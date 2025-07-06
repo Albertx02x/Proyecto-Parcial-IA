c4b89e0b0ec9ea0ce771283f559d334c1b5e6d29
Albertx02x <albertmedinafamilia@gmail.com>
2025-07-06 01:29:26 -0400

    Se corrigio las rutas de los sprites y sonidos

--- proyecto_ia/scripts/sprites.py
+++ proyecto_ia/scripts/sprites.py
@@ -6,29 +6,29 @@ import pygame
 #Sonidos
 pygame.mixer.init()
 try:
-    pygame.mixer.music.load("assets\\musica\\musica.mp3")
+    pygame.mixer.music.load("proyecto_ia\\assets\\musica\\musica.mp3")
     pygame.mixer.music.play(-1)
-    disparo_sonido = pygame.mixer.Sound("assets\\sonido\\disparo.wav")
-    game_over_sound = pygame.mixer.Sound("assets\\sonido\\game_over.wav")
-    nivel_completado_sound = pygame.mixer.Sound("assets\\sonido\\nivel_completado.wav")
-    juego_completado_sound = pygame.mixer.Sound("assets\\sonido\\juego_completado.wav")
+    disparo_sonido = pygame.mixer.Sound("proyecto_ia\\assets\\sonido\\disparo.wav")
+    game_over_sound = pygame.mixer.Sound("proyecto_ia\\assets\\sonido\\game_over.wav")
+    nivel_completado_sound = pygame.mixer.Sound("proyecto_ia\\assets\\sonido\\nivel_completado.wav")
+    juego_completado_sound = pygame.mixer.Sound("proyecto_ia\\assets\\sonido\\juego_completado.wav")
 except:
     print("No se encontraron archivos de sonido")
 
 #Sprites
 jugador_sprites = {
-    "arriba": pygame.image.load("assets\\images\\jugador\\arriba.png"),
-    "abajo": pygame.image.load("assets\\images\\jugador\\abajo.png"),
-    "izquierda": pygame.image.load("assets\\images\\jugador\\izquierda.png"),
-    "derecha": pygame.image.load("assets\\images\\jugador\\derecha.png")
+    "arriba": pygame.image.load("proyecto_ia\\assets\\images\\jugador\\arriba.png"),
+    "abajo": pygame.image.load("proyecto_ia\\assets\\images\\jugador\\abajo.png"),
+    "izquierda": pygame.image.load("proyecto_ia\\assets\\images\\jugador\\izquierda.png"),
+    "derecha": pygame.image.load("proyecto_ia\\assets\\images\\jugador\\derecha.png")
 }
 
 proyectil_sprites = {
-    "arriba": pygame.image.load("assets\\images\\disparo\\arriba.png"),
-    "abajo": pygame.image.load("assets\\images\\disparo\\abajo.png"),
-    "izquierda": pygame.image.load("assets\\images\\disparo\\izquierda.png"),
-    "derecha": pygame.image.load("assets\\images\\disparo\\derecha.png")
+    "arriba": pygame.image.load("proyecto_ia\\assets\\images\\disparo\\arriba.png"),
+    "abajo": pygame.image.load("proyecto_ia\\assets\\images\\disparo\\abajo.png"),
+    "izquierda": pygame.image.load("proyecto_ia\\assets\\images\\disparo\\izquierda.png"),
+    "derecha": pygame.image.load("proyecto_ia\\assets\\images\\disparo\\derecha.png")
 }
 
-enemigo_imagen = pygame.image.load("assets\\images\\enemigo.png")
-salida_imagen = pygame.image.load("assets\\images\\salida.png")
+enemigo_imagen = pygame.image.load("proyecto_ia\\assets\\images\\enemigo.png")
+salida_imagen = pygame.image.load("proyecto_ia\\assets\\images\\salida.png")
